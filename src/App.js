62b4cae4bd20b8048d1c1918866c05d8802b714b
Martin Javorský <62892540+Mart1n991@users.noreply.github.com>
2020-10-10 18:44:04 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [input, setInput] = useState("");
+  const [storedInputs, setStoredInputs] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const addColumn = (input) => {
+    if (input.length <= 0) {
+      return window.alert("Produkt musí mať aspoň 1 znak");
+    }
+
+    setStoredInputs([...storedInputs, input]);
+    setInput("");
+  };
+
+  const onChange = (event) => {
+    setSearch(([event.target.name] = event.target.value));
+  };
+
+  let filteredProducts = storedInputs.filter((product) => {
+    return product.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+  });
+
+  return (
+    <div className="App">
+      <div className="input-container">
+        <div className="row">
+          <label>Enter product</label>
+          <input
+            type="text"
+            value={input}
+            name="input"
+            onChange={onInputChange}
+          ></input>
+        </div>
+
+        <div className="row">
+          <label>Search</label>
+          <input
+            type="text"
+            value={search}
+            name="search"
+            onChange={onChange}
+          ></input>
+        </div>
+      </div>
+
+      <div className="button-container">
+        <button onClick={() => addColumn(input)}>Add column</button>
+        <button>Edit Label</button>
+      </div>
+
+      <table className="table">
+        <tbody>
+          <tr className="table-row">
+            <th>ID</th>
+            <th>Name</th>
+          </tr>
+
+          {filteredProducts.map((input, index) => {
+            return (
+              <tr key={index} className="table-row">
+                <td>{index + 1}</td>
+                <td>{input}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
